0770c2baea8c5dbea0c0ffe721d2c78c9113fa4d
safwanvk <safwanvalakundil@gmail.com>
2021-03-01 18:33:57 +0530

    :sparkles: Setup GetRoom

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import include, path
-from api.views import RoomView, CreateRoomView
+from api.views import RoomView, CreateRoomView, GetRoom
 
 urlpatterns = [
     path('', RoomView.as_view(), name='room_view'),
-    path('create-room', CreateRoomView.as_view(), name='create_room')
+    path('create-room', CreateRoomView.as_view(), name='create_room'),
+    path('get-room', GetRoom.as_view()),
 ]
