e90126199ec160d042c1cf0d3d7e7682c61ba437
safwanvk <safwanvalakundil@gmail.com>
2021-03-01 19:42:39 +0530

    :sparkles: Setup UpdateRoom

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import include, path
-from api.views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
+from api.views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
     path('', RoomView.as_view(), name='room_view'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
     path('user-in-room', UserInRoom.as_view()),
-    path('leave-room', LeaveRoom.as_view())
+    path('leave-room', LeaveRoom.as_view()),
+    path('update-room',UpdateRoom.as_view())
 ]
