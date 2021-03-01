3aa9419d73e57b99a31646acc4877c2f91f8c3df
safwanvk <safwanvalakundil@gmail.com>
2021-03-01 19:29:28 +0530

    :sparkles: Setup LeaveRoom

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import include, path
-from api.views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from api.views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 urlpatterns = [
     path('', RoomView.as_view(), name='room_view'),
     path('create-room', CreateRoomView.as_view(), name='create_room'),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
-    path('user-in-room', UserInRoom.as_view())
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view())
 ]
