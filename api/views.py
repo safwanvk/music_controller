30fd0fa11c114acb7f1a6f7542859797cee46dca
safwanvk <safwanvalakundil@gmail.com>
2021-03-01 18:52:40 +0530

    :sparkles: Setup UserInRoom

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from api.models import Room
 from api.serializer import RoomSerializer, CreateRoomSerializer
+from django.http import JsonResponse
 
 # Create your views here.
 class RoomView(generics.ListAPIView):
@@ -72,3 +73,14 @@ class CreateRoomView(APIView):
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
