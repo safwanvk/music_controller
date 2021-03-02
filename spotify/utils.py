5e99018dfa25fdc1640f0724ab35ae0ee9ab92d5
safwanvk <safwanvalakundil@gmail.com>
2021-03-02 10:06:31 +0530

    :sparkles: Add refresh_spotify_token

--- spotify/utils.py
+++ spotify/utils.py
@@ -27,3 +27,21 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
         tokens = SpotifyToken(user=session_id, access_token=access_token,
                               refresh_token=refresh_token, token_type=token_type, expires_in=expires_in)
         tokens.save()
+
+def refresh_spotify_token(session_id):
+    refresh_token = get_user_tokens(session_id).refresh_token
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    expires_in = response.get('expires_in')
+    refresh_token = response.get('refresh_token')
+
+    update_or_create_user_tokens(
+        session_id, access_token, token_type, expires_in, refresh_token)
