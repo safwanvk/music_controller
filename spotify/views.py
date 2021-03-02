7dd2321c51b2dff0f9cac3daca7feba19f629c10
safwanvk <safwanvalakundil@gmail.com>
2021-03-02 11:36:29 +0530

    :sparkles: Setup PauseSong

--- spotify/views.py
+++ spotify/views.py
@@ -5,7 +5,7 @@ from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
 from api.models import Room
-from spotify.utils import update_or_create_user_tokens, is_spotify_authenticated, execute_spotify_api_request
+from spotify.utils import update_or_create_user_tokens, is_spotify_authenticated, execute_spotify_api_request, pause_song
 
 # Create your views here.
 class AuthURL(APIView):
@@ -97,3 +97,13 @@ class CurrentSong(APIView):
         }
 
         return Response(song, status=status.HTTP_200_OK)
+
+class PauseSong(APIView):
+    def put(self, response, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)[0]
+        if self.request.session.session_key == room.host or room.guest_can_pause:
+            pause_song(room.host)
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({}, status=status.HTTP_403_FORBIDDEN)
